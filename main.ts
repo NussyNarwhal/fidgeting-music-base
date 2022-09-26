e7dad577e8024faf63b7d38a890c9fc5d544abce
NussyNarwhal <98988961+NussyNarwhal@users.noreply.github.com>
2022-09-26 14:35:44 -0600

    simultaneous buttons added

--- main.ts
+++ main.ts
@@ -1,17 +1,3 @@
-input.onPinPressed(TouchPin.P0, function () {
-    music.playMelody("A A A F D F A - ", 300)
-    strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
-    strip.setBrightness(255)
-    strip.showRainbow(1, 360)
-})
-input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index < 1; index++) {
-        music.playMelody("C D A C5 A C5 A F ", 800)
-        music.playMelody("D C D F C C C5 C5 ", 109)
-        strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
-        strip.setBrightness(255)
-    }
-})
 input.onButtonPressed(Button.B, function () {
     for (let index = 0; index < 1; index++) {
         music.playMelody("A F A F A F E D ", 300)
@@ -24,19 +10,38 @@ input.onButtonPressed(Button.B, function () {
 let strip: neopixel.Strip = null
 basic.showIcon(IconNames.EigthNote)
 music.playMelody("A F E D C B C5 C5 ", 400)
-for (let index = 0; index <= 11; index++) {
-    strip = neopixel.create(DigitalPin.P0, 12, NeoPixelMode.RGB)
+for (let index = 0; index <= 47; index++) {
+    strip = neopixel.create(DigitalPin.P0, 48, NeoPixelMode.RGB)
     strip.setBrightness(100)
     strip.setPixelColor(index, neopixel.colors(NeoPixelColors.Red))
     strip.show()
     basic.pause(100)
 }
+pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P5, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P11, PinPullMode.PullUp)
 basic.forever(function () {
-    music.playMelody("C D A - C - D - ", 400)
+    if (pins.digitalReadPin(DigitalPin.P1) == 0 && pins.digitalReadPin(DigitalPin.P1) == 0) {
+        music.playMelody("E A A F D F A - ", 300)
+        for (let index = 0; index < 60; index++) {
+            strip.showRainbow(1, 360)
+            strip.show()
+        }
+    } else if (pins.digitalReadPin(DigitalPin.P5) == 0 && pins.digitalReadPin(DigitalPin.P11) == 0) {
+        music.playMelody("C A C C D C C - ", 300)
+        for (let index = 0; index < 60; index++) {
+            strip.showRainbow(1, 360)
+            strip.show()
+        }
+    } else {
+    	
+    }
 })
 basic.forever(function () {
-    for (let index = 0; index <= 11; index++) {
-        strip.setPixelColor(index, neopixel.rgb(randint(8, 10), randint(0, 10), randint(8, 10)))
+    for (let index = 0; index <= 47; index++) {
+        strip.setPixelColor(index, neopixel.rgb(randint(8, 47), randint(0, 47), randint(8, 47)))
         strip.show()
     }
+    music.playMelody("C D A - C - D - ", 400)
 })
