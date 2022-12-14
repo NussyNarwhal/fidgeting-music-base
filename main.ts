be98ef41ee515f01e273f9e9df7689bc3b600a99
NussyNarwhal <98988961+NussyNarwhal@users.noreply.github.com>
2022-12-13 18:25:10 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,63 +1,58 @@
 let strip: neopixel.Strip = null
+pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P5, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P11, PinPullMode.PullUp)
 for (let index = 0; index <= 47; index++) {
-    strip = neopixel.create(DigitalPin.P0, 48, NeoPixelMode.RGB)
+    strip = neopixel.create(DigitalPin.P8, 48, NeoPixelMode.RGB)
     strip.setBrightness(100)
     strip.setPixelColor(index, neopixel.colors(NeoPixelColors.Red))
     strip.show()
     basic.pause(16)
 }
 for (let index = 0; index <= 47; index++) {
-    strip = neopixel.create(DigitalPin.P0, 48, NeoPixelMode.RGB)
+    strip = neopixel.create(DigitalPin.P8, 48, NeoPixelMode.RGB)
     strip.setBrightness(100)
     strip.setPixelColor(index, neopixel.colors(NeoPixelColors.Green))
     strip.show()
     basic.pause(16)
 }
-for (let index = 0; index <= 47; index++) {
-    strip = neopixel.create(DigitalPin.P0, 48, NeoPixelMode.RGB)
-    strip.setBrightness(100)
-    strip.setPixelColor(index, neopixel.colors(NeoPixelColors.Blue))
-    strip.show()
-    basic.pause(16)
-}
-pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
-pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
-pins.setPull(DigitalPin.P5, PinPullMode.PullUp)
-pins.setPull(DigitalPin.P11, PinPullMode.PullUp)
-basic.showIcon(IconNames.Yes)
 basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P1) == 0 && pins.digitalReadPin(DigitalPin.P2) == 0) {
-        for (let index = 0; index < 60; index++) {
-            strip.showRainbow(80, 90)
+    if (pins.digitalReadPin(DigitalPin.P1) == 0 && pins.digitalReadPin(DigitalPin.P5) == 0) {
+        for (let index = 0; index <= 47; index++) {
+            strip.setPixelColor(index, neopixel.rgb(255, 255, 255))
             strip.show()
         }
-    } else if (pins.digitalReadPin(DigitalPin.P5) == 0 && pins.digitalReadPin(DigitalPin.P11) == 0) {
-        for (let index = 0; index < 60; index++) {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
+    } else if (pins.digitalReadPin(DigitalPin.P2) == 0 && pins.digitalReadPin(DigitalPin.P11) == 0) {
+        for (let index = 0; index < 2; index++) {
             strip.showRainbow(100, 120)
             strip.show()
         }
-    }
-    if (pins.digitalReadPin(DigitalPin.P1) == 0) {
-        for (let index = 0; index < 60; index++) {
+    } else if (false) {
+    	
+    } else if (pins.digitalReadPin(DigitalPin.P1) == 0) {
+        for (let index = 0; index < 2; index++) {
             strip.showRainbow(180, 200)
             strip.show()
         }
     } else if (pins.digitalReadPin(DigitalPin.P2) == 0) {
-        for (let index = 0; index < 60; index++) {
+        for (let index = 0; index < 2; index++) {
             strip.showRainbow(231, 260)
             strip.show()
         }
-    }
-    if (pins.digitalReadPin(DigitalPin.P5) == 0) {
-        for (let index = 0; index < 60; index++) {
+    } else if (pins.digitalReadPin(DigitalPin.P5) == 0) {
+        for (let index = 0; index < 2; index++) {
             strip.showRainbow(330, 360)
             strip.show()
         }
     } else if (pins.digitalReadPin(DigitalPin.P11) == 0) {
-        for (let index = 0; index <= 47; index++) {
+        for (let index = 0; index <= 2; index++) {
             strip.showRainbow(11, 30)
             strip.show()
         }
+    } else {
+        music.stopAllSounds()
     }
 })
 basic.forever(function () {
@@ -65,4 +60,5 @@ basic.forever(function () {
         strip.setPixelColor(index, neopixel.rgb(randint(8, 47), randint(0, 47), randint(8, 47)))
         strip.show()
     }
+    music.playMelody("C C5 A - C - D - ", 400)
 })
